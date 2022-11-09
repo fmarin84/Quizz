e80be40f6bc436053e917062cbe381de5193705e
Snowaice <thibaud.lambert31@gmail.com>
2022-11-09 11:01:36 +0100

    QuestionAPi

---
+++ src/main/java/com/example/quizz/controller/QuestionApi.java
@@ -0,0 +1,40 @@
+package com.example.quizz.controller;
+
+import com.example.quizz.entity.Question;
+import com.example.quizz.repository.QuestionRepository;
+import com.example.quizz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api")
+public class QuestionApi {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("question/{id}")
+    public ResponseEntity  getQuestion(@PathVariable("id") int id){
+
+        Optional<Question> question = questionService.getQuestion(id);
+        if(question.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }else{
+            return ResponseEntity.ok().body(question.get());
+        }
+    }
+
+
+    @GetMapping("questions/{id}")
+    public List<Question> getQuestions(@PathVariable("id") int id){
+        return questionService.getQuestions(id);
+    }
+
+}
