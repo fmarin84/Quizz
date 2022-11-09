68fb4fb71f48cf808b498aafc27e8f3c809f0332
Snowaice <thibaud.lambert31@gmail.com>
2022-11-09 10:46:06 +0100

    refact arborescence

--- src/main/java/com/example/quizz/controller/QuizzApi.java
+++ src/main/java/com/example/quizz/controller/QuizzApi.java
@@ -1,9 +1,10 @@
-package com.example.quizz.apiController;
+package com.example.quizz.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
-public class QuizzApiController {
+public class QuizzApi {
+
 }
