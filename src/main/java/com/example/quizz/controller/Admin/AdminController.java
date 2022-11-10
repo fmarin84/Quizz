ffbe10c680370dbc7605a586e5f5a8b0e007b872
fmarin84 <fabien.marin84@gmail.com>
2022-11-10 14:21:57 +0100

    admin Crud answer

--- src/main/java/com/example/quizz/controller/Admin/AdminController.java
+++ src/main/java/com/example/quizz/controller/Admin/AdminController.java
@@ -1,8 +1,10 @@
 package com.example.quizz.controller.Admin;
 
+import com.example.quizz.entity.Answer;
 import com.example.quizz.entity.Question;
 import com.example.quizz.entity.Quizz;
 import com.example.quizz.entity.User;
+import com.example.quizz.service.AnswerService;
 import com.example.quizz.service.QuestionService;
 import com.example.quizz.service.QuizzService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class AdminController {
     
@@ -19,6 +23,12 @@ public class AdminController {
     private QuizzService quizzService;
     @Autowired
     private QuestionService questionService;
+
+//    @Autowired
+//    private UserService userService;
+
+    @Autowired
+    private AnswerService answerService;
 
     /*  Quizz */
     @GetMapping("quizz")
@@ -102,7 +112,62 @@ public class AdminController {
         return "question.html";
     }
 
+    /* User */
+//    @GetMapping("user")
+//    public String getUserView(Model model){
+//        model.addAttribute("users", userService.getAll());
+//
+//        return "user.html";
+//    }
+//
+//    @PostMapping("newUser")
+//    public String add(User user, Model model){
+//        userService.add(user);
+//        model.addAttribute("users", userService.getAll());
+//
+//        return "user.html";
+//    }
 
+    /* Answer */
+    @GetMapping("answer")
+    public String getAnswerView(Model model){
+        model.addAttribute("answers", answerService.getAll());
+        model.addAttribute("questions", questionService.getAll());
 
-    
+        return "answer.html";
+    }
+
+    @PostMapping("newAnswer")
+    public String addAnswer(Answer answer, Model model){
+        answerService.add(answer);
+        model.addAttribute("answers", answerService.getAll());
+        model.addAttribute("questions", questionService.getAll());
+
+        return "answer.html";
+    }
+
+    @GetMapping("updateAnswer/{id}")
+    public String getUpdateAnswerView(@PathVariable("id") int id, Model model){
+        model.addAttribute("answer", answerService.getAnswer(id).get());
+        model.addAttribute("questions", questionService.getAll());
+
+        return "updateAnswer.html";
+    }
+
+    @PostMapping("updateAnswer")
+    public String updateAnswer(Answer answer, Model model) throws Exception {
+        answerService.update(answer.getId(), answer);
+        model.addAttribute("answers", answerService.getAll());
+
+        return "answer.html";
+    }
+
+    @PostMapping("deleteAnswer/{id}")
+    public String deleteAnswer(@PathVariable("id") int id, Model model){
+        answerService.deleteAnswer(id);
+        model.addAttribute("answers", answerService.getAll());
+        model.addAttribute("questions", questionService.getAll());
+
+        return "answer.html";
+    }
 }
--- src/main/java/com/example/quizz/service/AnswerService.java
+++ src/main/java/com/example/quizz/service/AnswerService.java
@@ -1,10 +1,7 @@
 package com.example.quizz.service;
 
 import com.example.quizz.entity.Answer;
-import com.example.quizz.entity.Question;
-import com.example.quizz.entity.User;
 import com.example.quizz.repository.AnswerRepository;
-import com.example.quizz.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,4 +31,31 @@ public class AnswerService {
     //public List<Answer> getAnswerList(int id){
     //    return answerRepository.findByUser_Id(id);
     //}
+
+    public void add(Answer anser){
+        answerRepository.save(anser);
+    }
+
+    public List<Answer> getAll(){ return answerRepository.findAll();}
+
+    public void deleteAnswer(int id) { answerRepository.deleteById(id);}
+
+    public Optional<Answer> getAnswer(int id){
+        return answerRepository.findById(id);
+    }
+
+    public void update(int id, Answer answer) throws Exception{
+        if(id != answer.getId())
+            throw new Exception();
+
+        Optional<Answer> q = answerRepository.findById(id);
+        if(q.isPresent()) {
+            //TODO Faire une fonction
+            q.get().setTitle(answer.getTitle());
+            q.get().setRightWrong(answer.isRightWrong());
+            answerRepository.save(answer);
+        } else {
+            throw new Exception();
+        }
+    }
 }
